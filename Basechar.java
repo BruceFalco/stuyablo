6eab4d8e497b9bce37893de953c994d89acb4c2f
Zafir Hasan <zafirhsn@gmail.com>
2014-10-18 21:42:20 -0400

    Zafir -Got the player to wander a random number of steps and put all basic player attributes in Basechar

--- Basechar.java
+++ Basechar.java
@@ -1,37 +1,75 @@
+import java.util.Random;
+
 /* superclass for warrior (base class) and Mage */
 public class Basechar {
-    private int health=20;
     private String name;
-    /*
-    public Basechar() {
-	setName("default");
-    }
-    */
-    public Basechar(String n) {
-	setName(n);
-    }
-    
-    public String toString(){
-	return this.getName();
-    }
-    
-    public void attack(Basechar other){
-	//System.out.println(this.getName()+" is attacking "+other.getName());
-	System.out.println(this+" is attacking "+other);
+    private int health = 100;
+    private int defense = 100;
+    private int mana = 0;
+    private int armor = 0;
 
+//-------------NAME--------------
+    public void setName(String n) {
+	name = n;
     }
-    
-    public void setName(String name){
-	this.name = name;
+    public String getName() {
+	return name;
     }
-    
-    public String getName(){
-	return this.name;
+//------------HEALTH------------ 
+    public void setHealth(int h) {
+	health = h;
     }
-
     public int getHealth() {
-	return this.health;
+	return health;
+    }
+//-----------DEFENSE------------
+    public void setDefense(int d) {
+	defense = d;
+    }
+    public int getDefense() {
+	return defense;
+    }
+//------------MANA--------------
+    public void setMana(int m) {
+	mana = m;
+    }
+    public int getMana() {
+	return mana;
+    }
+//------------ARMOR------------
+    public void setArmor(int a) {
+	armor = a;
+    }
+    public int getArmor() {
+	return armor;
+    }
+   
+    /*Zafir
+      -Got Wander to work
+      -Player "walks" for a random number of steps between 1 and 25
+      -There is a gap of 0.5 seconds b/w each step
+      -Message pops up that notifies player that they have encountered an enemy
+
+     Next Steps
+      -Give the player an option to fight or run
+      -Generate a random type of CPU
+      -Randomly pick up items */
+    public void Wander() {
+	Random rand = new Random();
+	for(int value = rand.nextInt(25)+1;value>0;value-=1){
+	    System.out.println("Step" + "\n");
+	    try {
+	    Thread.sleep(500);
+	    } catch (InterruptedException e) {
+		 e.printStackTrace();
+		}
+	}	
+	System.out.println("You have encountered a wild warrior!");
+	Centurion CPU = new Centurion();
     }
 
     
+
+
+
 }
---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	Centurion player = new Centurion();
+	
+	player.Wander();
+    }
+
+}
