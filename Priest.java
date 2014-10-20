127f8a64362bb28acf228acffe91d66ec6456896
tjieluo.capri <tjieluo.capri@gmail.com>
2014-10-20 09:27:27 -0400

    Timon

---
+++ Priest.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.io.IOException;
+
+public class Priest extends Basechar {
+    private int mana = 100;
+    
+    // CONSTRUCTORS
+    public Priest (String n) {
+	setName(n);
+    }
+    public Priest () {
+	setName("Faustus");
+    }
+
+}
